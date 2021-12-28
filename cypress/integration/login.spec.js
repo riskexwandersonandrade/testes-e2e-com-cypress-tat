33363680d669fde270245d5529806dc24e9a172c
Wanderson Andrade <wanderson.andrade@riskex.com.br>
2021-12-28 11:40:03 -0300

    Create e2e tests with cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,15 @@
+/// <reference path="../support/commands.d.ts" />
+
+it('successfully logs in', () => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+
+  cy.login(
+    Cypress.env('USER_EMAIL'),
+    Cypress.env('USER_PASSWORD'),
+    { cacheSession: false }
+  )
+
+  cy.wait('@getNotes')
+
+  cy.log('log 123 continuous integration')
+})
